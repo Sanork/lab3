59ba009bd8b9a4407230fd6a01df568126f757fa
Sanork <71247980+Sanork@users.noreply.github.com>
2021-10-29 05:55:02 +0700

    Added weapon and armor

--- Character.cpp
+++ Character.cpp
@@ -5,4 +5,42 @@
 
 using namespace std;
 
-void Character::char_1 (string a, int b, string c, string a1, int b1, string c1, int d1, string a2, int b2, int c2, int a3, int b3, int c3, int d3, int e3, int a31, int b31, int c31) /*Создание персонажа*/
+void Character::char_1(string a, int b, string c, string a1, int b1, string c1, int d1, string a2, int b2, int c2, int a3, int b3, int c3, int d3, int e3, int a31, int b31, int c31) /*Создание персонажа*/
+{
+	//Character ch;
+	name = a;
+	lvl = b;
+	race = c;
+	arm = arm_1(a1, b1, c1, d1);
+	weap = weap_1(a2, b2, c2);
+	stat = stat_1(a3, b3, c3, d3, e3, a31, b31, c31);
+	//return ch;
+}
+
+void Character::SetChar() /*Создание персонажа вручную с консоли*/
+{
+	//character ch = {};
+	cout << "Введите имя персонажа" << endl;
+	cin >> name;
+	cout << "Введите уровень персонажа" << endl;
+	cin >> lvl;
+	cout << "Введите расу персонажа" << endl;
+	cin >> race;
+	arm = SetArm();
+	weap = SetWeap();
+	stat = SetStats();
+	//return ch;
+}
+
+void Character::GetChar() /*Вывод информации о персонаже*/
+{
+	cout << "Персонаж: ";
+	cout << name << endl;
+	cout << "Уровень - ";
+	cout << lvl << endl;
+	cout << "Раса - ";
+	cout << race << endl;
+	GetArm();
+	GetWeap();
+	GetStats();
+}
